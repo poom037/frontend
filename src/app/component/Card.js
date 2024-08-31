954e14b0ffd00659349d802aa45e95e2884e9965
poom037 <authaipat3356@gmail.com>
2024-09-01 02:20:50 +0700

    IOOO

--- src/app/component/Card.js
+++ src/app/component/Card.js
@@ -26,7 +26,7 @@ export default function Carousel() {
               <Image src={slider6} className="card-img-top" alt="Card 2" width={640} height={480} />
               <div className="card-body">
                 <h5 className="card-title">Chathai</h5>
-                <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p className="card-text">Lorem Ipsum is simply dummy text of the printing and ustry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
               </div>
               <div className="card-footer">
                 <small className="text-body-secondary">Last updated 3 mins ago</small>
