db9f837841bebe2763b662146b46e88de990e1ec
poom037 <authaipat3356@gmail.com>
2024-09-01 16:17:40 +0700

    poi

--- src/app/component/navbar.js
+++ src/app/component/navbar.js
@@ -36,7 +36,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">สายการบินไปด้วยกันมั้ย</a>
+        <a className="navbar-brand" href="/">ร้านชาไทย</a>
         <button
           className="navbar-toggler"
           type="button"
