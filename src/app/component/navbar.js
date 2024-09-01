fb5bc406a008e2086b456aae60870db39b518737
poom037 <authaipat3356@gmail.com>
2024-09-01 15:51:00 +0700

    fin

--- src/app/component/navbar.js
+++ src/app/component/navbar.js
@@ -1,10 +1,7 @@
-// components/Navbar.js
 'use client';
-import { useState, useEffect } from 'react';
+
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import logo from '../../../public/slider5.jpg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
@@ -37,18 +34,24 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link href="/" className="navbar-brand">
-          <Image src={logo} alt="Logo" width={80} height={80} className="d-inline-block align-text-top" />
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <a className="navbar-brand" href="/">สายการบินไปด้วยกันมั้ย</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+              <Link className="nav-link" href="/">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" href="/about">About</Link>
@@ -59,22 +62,19 @@ export default function Navbar() {
             <li className="nav-item">
               <Link className="nav-link" href="/contact">Contact</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="/users">User Info</Link>
-            </li>
           </ul>
-          <div className="d-flex ms-auto mb-2 mb-lg-0">
-            {!isLoggedIn ? (
-              <>
-                <Link href="/signup">
-                  <button className="btn btn-outline-primary me-2">SignUp</button>
-                </Link>
-                <Link href="/signin">
-                  <button className="btn btn-outline-primary">SignIn</button>
-                </Link>
-              </>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+          <div className="d-flex ms-auto">
+            {isLoggedIn ? (
+              <button className="btn btn-outline-danger me-2" onClick={handleLogout}>Logout</button>
             ) : (
-              <button className="btn btn-outline-danger" onClick={handleLogout}>Logout</button>
+              <>
+                <Link className="btn btn-outline-primary me-2" href="/signin">Sign In</Link>
+                <Link className="btn btn-primary" href="/signup">Sign Up</Link>
+              </>
             )}
           </div>
         </div>
@@ -82,4 +82,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
